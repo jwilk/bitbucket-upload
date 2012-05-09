e2085888c5c3e093e7c11bb1dad9d3b34b462a08
Hong Minhee <minhee@dahlia.kr>
2012-05-09 15:52:54 +0900

    Description and long description

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 try:
     from setuptools import setup
 except ImportError:
@@ -8,8 +9,20 @@ from bitbucket_distutils import __author__, __email__, __license__, __version__
 under_270 = sys.version_info < (2, 7, 0)
 
 
+def readme():
+    try:
+        with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+            return f.read()
+    except (IOError, OSError):
+        return ''
+
+
 setup(name='bitbucket-distutils',
+      description='Distribute/setuptools/distutils command for Bitbucket. '
+                  'You can use Bitbucket downloads instead of PyPI downloads '
+                  'for release.',
       version=__version__,
+      long_description=readme(),
       py_modules=['bitbucket_distutils'],
       author=__author__,
       author_email=__email__,
